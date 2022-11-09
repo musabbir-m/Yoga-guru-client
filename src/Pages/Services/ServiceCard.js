bf67881aa20f8c661454d833a1aa307edf954bb3
Musabbir-pr <musabbir313@gmail.com>
2022-11-09 10:59:20 +0600

    added card details and see all button

--- src/Pages/Services/ServiceCard.js
+++ src/Pages/Services/ServiceCard.js
@@ -3,18 +3,33 @@ import React from "react";
 const ServiceCard = ({ data }) => {
   const { title, img, _id, price, level, description } = data;
   return (
-    <div className="card w-96 bg-base-100 shadow-lg image-full">
+    <div className="card card-compact w-96 bg-base-100 shadow-xl mx-auto py-5 my-2">
       <figure>
-        <img src={img} alt="" />
+        <img className="w-full" src={img} alt="" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
         <p>{description}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-info text-white	border-0 rounded-3xl">
+            See Details
+          </button>
         </div>
       </div>
     </div>
+
+    // <div className="card w-96 bg-base-100 shadow-lg image-full">
+    //   <figure>
+    //     <img src={img} alt="" />
+    //   </figure>
+    //   <div className="card-body">
+    //     <h2 className="card-title">{title}</h2>
+    //     <p>{description.slice(0,100)} ...</p>
+    //     <div className="card-actions justify-end">
+    //       <button className="btn btn-primary">Buy Now</button>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,23 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import ServiceCard from './ServiceCard';
+import React, { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
 
 const Services = () => {
-    const [services, setServices]= useState([])
-    useEffect(()=>{
-        fetch('http://localhost:5000/services')
-        .then(res=>res.json())
-        .then(data=> setServices(data))
-    },[])
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/services")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
 
-    return (
-        <div>
-            {services.map(service=>
-            <ServiceCard
-            key={service._id}
-            data={service}
-            ></ServiceCard>)}
-        </div>
-    );
+  return (
+    <div className="my-10 ">
+      <h2 className="text-5xl py-10 text-center">Available Trainings</h2>
+      <div className="grid grid-cols-1 lg:grid-cols-3 mx-auto">
+        {services.map((service) => (
+          <ServiceCard key={service._id} data={service}></ServiceCard>
+        ))}
+      </div>
+      <div className="flex">
+      <button className="btn btn-active bg-pink-500 border-0 hover:bg-pink-400 rounded-3xl w-48 mx-auto">See All</button>
+      </div>
+     
+    </div>
+  );
 };
 
 export default Services;
